65049ef7fbc6b4bc17034e2a0ad66d3fc1cb0c7f
vk <VKorelsky@users.noreply.github.com>
2025-08-17 11:47:01 +0100

    rejig frontend mounting structure

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse, RedirectResponse
 from pathlib import Path
 
 from .routes import sessions
@@ -18,18 +19,20 @@ app.add_middleware(
 # routes
 app.include_router(sessions.router, prefix="/api")
 
+# mount frontned
+frontend_build_dir = Path(__file__).parent.parent / "fe" / "build"
+assets_dir = frontend_build_dir / "_app"
 
-@app.get("/")
-async def root():
-    return {"message": "Server is running!"}
+if assets_dir.exists():
+    app.mount("/_app", StaticFiles(directory=assets_dir), name="assets")
+
+# root index
+@app.get("/", include_in_schema=False)
+def serve_index():
+    return FileResponse(frontend_build_dir / "index.html")
 
 
-# mount compiled frontend
-prerendered_dir = Path(__file__).parent.parent / "fe" / ".svelte-kit" / "output" / "prerendered" / "pages"
-client_dir = Path(__file__).parent.parent / "fe" / ".svelte-kit" / "output" / "client"
-
-if client_dir.exists():
-    app.mount("/_app", StaticFiles(directory=client_dir / "_app"), name="assets")
-
-if prerendered_dir.exists():
-    app.mount("/", StaticFiles(directory=prerendered_dir, html=True), name="pages")
+# Fallback for client-side routing
+@app.get("/{full_path:path}", include_in_schema=False)
+def spa_fallback():
+    return FileResponse(frontend_build_dir / "index.html")
